8ff4a5fd9e5847c03e84dad9958ca39755a44e62
Janani216 <47896137+Janani216@users.noreply.github.com>
2021-10-30 03:19:20 -0400

    Update lr_fraud_prediction.py

--- lr_fraud_prediction.py
+++ lr_fraud_prediction.py
@@ -83,6 +83,6 @@ def LR(x_train,y_train,x_test,y_test):
   test_acc = metrics.accuracy_score(y_train, x_pred)
   model = logisticRegr.get_params
 
-  return model, train_acc , test_acc
+  return model, round(train_acc,2) , round(test_acc,2)
 
 LR(x_train,y_train,x_test,y_test)
